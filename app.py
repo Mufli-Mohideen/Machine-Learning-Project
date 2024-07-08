5020a65496e0cb4ad34b68033a371c7aba953532
Mufli-Mohideen <mufli2mail@gmail.com>
2024-07-08 16:49:47 +0530

    Project Completed

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify, render_template
 import pickle
 import pandas as pd
+import json
 from itertools import combinations
 
 app = Flask(__name__)
@@ -84,6 +85,38 @@ venue_name_mapping = {
     'Bharat Ratna Shri Atal Bihari Vajpayee Ekana Cricket Stadium, Lucknow': 'Bharat Ratna Shri Atal Bihari Vajpayee Ekana Cricket Stadium'
 }
 
+
+# bowler_data = {
+#     'Player': ['B Kumar', 'SP Narine', 'R Ashwin', 'YS Chahal', 'PP Chawla', 'JJ Bumrah', 'RA Jadeja', 'Rashid Khan', 'UT Yadav', 'Sandeep Sharma',
+#                'HV Patel', 'MM Sharma', 'Mohammed Shami', 'AR Patel', 'TA Boult', 'K Rabada', 'AD Russell', 'SN Thakur', 'Mohammed Siraj', 'Kuldeep Yadav',
+#                'CV Varun', 'DL Chahar', 'L Balaji', 'KH Pandya', 'RD Chahar', 'Avesh Khan', 'T Natarajan', 'HH Pandya', 'Ravi Bishnoi', 'Shakib Al Hasan',
+#                'PJ Cummins', 'Mustafizur Rahman', 'A Nortje', 'JO Holder', 'S Gopal', 'MA Starc', 'LH Ferguson', 'VR Aaron', 'MP Stoinis', 'AJ Tye',
+#                'TU Deshpande', 'MR Marsh', 'M Markande', 'MS Gony', 'GJ Maxwell', 'Washington Sundar'],
+#     'Matches': [176, 177, 212, 160, 192, 133, 240, 121, 148, 127,
+#                 106, 112, 110, 150, 104, 80, 127, 95, 93, 84,
+#                 71, 81, 73, 127, 78, 63, 61, 137, 66, 71,
+#                 58, 57, 46, 46, 52, 41, 45, 52, 96, 30,
+#                 36, 42, 37, 44, 134, 60],
+#     'Balls': [3910, 4075, 4524, 3521, 3850, 3053, 3829, 2859, 3050, 2822,
+#               2161, 2253, 2426, 3099, 2337, 1818, 1697, 1866, 1958, 1746,
+#               1588, 1669, 1513, 2122, 1664, 1335, 1337, 1418, 1411, 1484,
+#               1319, 1298, 1054, 1036, 1061, 871, 1155, 1173, 1823, 550,
+#               684, 744, 703, 878, 2693, 1148],
+#     'Runs': [1153, 3603, 2727, 2704, 2588, 2671, 3132, 2132, 3444, 2652,
+#              1898, 2050, 2388, 2980, 2151, 1564, 1830, 1747, 1827, 1771,
+#              1627, 1697, 1682, 2223, 1784, 1516, 1575, 1750, 1468, 1572,
+#              1236, 1255, 1041, 1037, 1062, 876, 1158, 1177, 1846, 563,
+#              686, 746, 705, 880, 2693, 1151],
+#     'Wickets': [171, 127, 260, 145, 160, 190, 240, 155, 153, 147,
+#                 118, 125, 133, 163, 121, 85, 97, 86, 82, 76,
+#                 74, 82, 76, 118, 91, 74, 82, 76, 76, 71,
+#                 72, 72, 61, 63, 68, 46, 55, 60, 104, 34,
+#                 40, 43, 36, 40, 115, 63]
+# }
+
+
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -140,6 +173,10 @@ def champions():
 
 # @app.route('/predict', methods=['POST'])
 # def predict():
+with open('bowler_data.json') as f:
+    bowler_data = json.load(f)
+df = pd.DataFrame(bowler_data)
+predicted_purple_cap_holder = df.loc[df['Wickets'].idxmax(), 'Player']
 #     try:
 #         data = request.form.to_dict()
 #         print("Received form data:", data)  # Debug statement to print received form data
@@ -150,8 +187,11 @@ def champions():
         
 #         # Validate team1, team2, and venue
 #         if data['team1'] not in teams:
-#             return jsonify({'error': f'Invalid team1: {data["team1"]}. Expected one of: {", ".join(teams)}'}), 400
-        
+#             return jsonify({'error': f'Invalid team1: {data["team1"]}. Expected one of: {", ".join(teams)}'}), 40
+with open('batsman_data.json') as f:
+    batsman_data = json.load(f)
+batsman_df = pd.DataFrame(batsman_data)
+predicted_orange_cap_holder = batsman_df.loc[batsman_df['Runs'].idxmax(), 'Player']
 #         if data['team2'] not in teams:
 #             return jsonify({'error': f'Invalid team2: {data["team2"]}. Expected one of: {", ".join(teams)}'}), 400
         
@@ -184,6 +224,14 @@ def champions():
     
 #     except Exception as e:
 #         return jsonify({'error': f'Server error: {str(e)}'}), 500
+
+@app.route('/predict-purple-cap')
+def predict_purple_cap():
+    return jsonify({'predicted_purple_cap_holder': predicted_purple_cap_holder})
+
+@app.route('/predict-orange-cap')
+def predict_orange_cap():
+    return jsonify({'predicted_orange_cap_holder': predicted_orange_cap_holder})
 
 
 @app.route('/predict-ipl-2025', methods=['GET'])
